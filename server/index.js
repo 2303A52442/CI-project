9ea6f35046a8c0072282835dd29b13ec00852ab4
2303A52442 <2303A52442@sru.edu.in>
2026-03-23 15:43:07 +0530

    CI pipeline added

--- server/index.js
+++ server/index.js
@@ -1,18 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Server is running!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
 });
-
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
-}
 
 module.exports = app;
--- server/test/app.test.js
+++ server/test/app.test.js
@@ -1,10 +1,11 @@
-const request = require('supertest');
-const app = require('../index'); // Adjust the path if necessary
+const request = require("supertest");
+const app = require("../index");
 
-describe('GET /', () => {
-    it('should return a message indicating the server is running', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('message', 'Server is running!');
-    });
+describe("GET /", () => {
+  it("should return server running", async () => {
+    const res = await request(app).get("/");
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toBe("Server is running!");
+  });
 });
